3006b0e684a2c68c9ba8f3d602f58c376779b916
arevbond <bondarev.n17@yandex.ru>
2023-09-11 16:40:53 +0300

    Add read advice bot by Igor Tuzov

---
+++ events/telegram/messages.go
@@ -0,0 +1,14 @@
+package telegram
+
+// TODO: add emoji in messages
+
+const msgHelp = `I can save and keep your pages`
+
+const msgHello = "Hi there\n\n" + msgHelp
+
+const (
+	msgUnknownCommand = "Unknown command"
+	msgNoSavedPages   = "You have no saved pages!"
+	msgSaved          = "Saved!"
+	msgAlreadyExists  = "You have already this page in your list"
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	tgClient "tg_ics_useful_bot/clients/telegram"
+	"tg_ics_useful_bot/consumer/event-consumer"
+	"tg_ics_useful_bot/events/telegram"
+	"tg_ics_useful_bot/storage/files"
+)
+
+const (
+	tgBotHost   = "api.telegram.org"
+	storagePath = "files_storage"
+	batchSize   = 100
+)
+
+func main() {
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		files.New(storagePath),
+	)
+
+	log.Print("service started")
+
+	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stopped", err)
+	}
+}
+
+func mustToken() string {
+	token := flag.String(
+		"tg-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
