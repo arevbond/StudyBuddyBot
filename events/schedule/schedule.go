455f5ef4f2b1baea2f022f657e679abafbd28ab7
arevbond <bondarev.n17@yandex.ru>
2023-11-03 15:04:06 +0300

    Add schedule func from google-calendar

--- events/schedule/schedule.go
+++ events/schedule/schedule.go
@@ -1,1 +1,49 @@
 package schedule
+
+import (
+	"fmt"
+	google_calendar "tg_ics_useful_bot/clients/google-calendar"
+	"time"
+)
+
+func Schedule(calendarID string) string {
+	result := "Расписание:\n\n"
+	for day := time.Monday; day <= time.Saturday; day++ {
+		cur := scheduleByDay(day, calendarID)
+		if cur != "" {
+			result += cur + "\n"
+		}
+	}
+	return result
+}
+
+func scheduleByDay(day time.Weekday, calendarID string) string {
+	dayToLessons := google_calendar.Lessons(calendarID)
+	result := ""
+	lessons := dayToLessons[day]
+
+	if len(lessons) == 0 {
+		return ""
+	}
+
+	switch day {
+	case time.Monday:
+		result += "Понедельник"
+	case time.Tuesday:
+		result += "Вторник"
+	case time.Wednesday:
+		result += "Среда"
+	case time.Thursday:
+		result += "Четверг"
+	case time.Friday:
+		result += "Пятница"
+	case time.Saturday:
+		result += "Суббота"
+	}
+	result += "\n"
+
+	for _, l := range lessons {
+		result += fmt.Sprintf("%s - %s\n", l.Name, l.DateTime.Format("15:04"))
+	}
+	return result
+}
