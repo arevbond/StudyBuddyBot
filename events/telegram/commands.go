b0f22d35ce93167efe2a70527e4eb1ef3a39f57e
arevbond <bondarev.n17@yandex.ru>
2023-11-04 20:00:18 +0300

    Change commands suffix name, add isCommand func

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -21,67 +21,70 @@ const (
 	sendPhotoMethod
 )
 
-var (
-	AllCmd      = []string{"/all", "/all@ics_useful_bot"}
-	AnecdotCmd  = []string{"/joke", "/joke@ics_useful_bot"}
-	FlipCmd     = []string{"/flip", "/flip@ics_useful_bot"}
-	GayStartCmd = []string{"/gay", "/gay@ics_useful_bot"}
-	GayTopCmd   = []string{"/top_gay", "/top_gay@ics_useful_bot"}
-	XkcdCmd     = []string{"/xkcd", "/xkcd@ics_useful_bot"}
-	DicStartCmd = []string{"/dick", "/dick@ics_useful_bot"}
-	DickTopCmd  = []string{"/top_dick", "/top_dick@ics_useful_bot"}
-	DickDuelCmd = []string{"/duel", "/duel@ics_useful_bot"}
-	ScheduleCmd = []string{"/schedule", "/schedule@ics_useful_bot"}
+const (
+	suffix = "@ics_useful_bot"
+)
 
-	AddCalendarIDCmd = []string{"/add_calendar", "/add_calendar@ics_useful_bot"}
+var (
+	AllCmd           = "/all"
+	AnecdotCmd       = "/joke"
+	FlipCmd          = "/flip"
+	GayStartCmd      = "/gay"
+	GayTopCmd        = "/top_gay"
+	XkcdCmd          = "/xkcd"
+	DicStartCmd      = "/dick"
+	DickTopCmd       = "/top_dick"
+	DickDuelCmd      = "/duel"
+	ScheduleCmd      = "/schedule"
+	AddCalendarIDCmd = "/add_calendar"
 )
 
 // selectCommand select one of available commands.
-func (p *Processor) selectCommand(text string, chat *telegram.Chat, user *telegram.User, messageID int) (string, method, error) {
+func (p *Processor) selectCommand(cmd string, chat *telegram.Chat, user *telegram.User, messageID int) (string, method, error) {
 	var message string
 	var mthd method
 	var err error
 	switch {
 
-	case utils.Equal(text, AllCmd):
+	case isCommand(cmd, AllCmd):
 		message = p.allUsernames(chat.ID)
 		mthd = sendMessageMethod
 
-	case utils.Equal(text, GayTopCmd):
+	case isCommand(cmd, GayTopCmd):
 		message, err = p.gameGayTop(chat.ID)
 		if err != nil {
 			return "", UnsupportedMethod, e.Wrap("can't do GayTopCmd: ", err)
 		}
 		mthd = sendMessageMethod
 
-	case utils.Equal(text, GayStartCmd):
+	case isCommand(cmd, GayStartCmd):
 		message, err = p.gameGay(chat.ID)
 		if err != nil {
 			return "", UnsupportedMethod, e.Wrap("can't get message from gameGay: ", err)
 		}
 		mthd = sendMessageMethod
 
-	case utils.Equal(text, DickTopCmd):
+	case isCommand(cmd, DickTopCmd):
 		message, err = p.topDick(chat.ID)
 		if err != nil {
 			return "", UnsupportedMethod, e.Wrap(fmt.Sprintf("can't get top dics from chat %d: ", chat.ID), err)
 		}
 		mthd = sendMessageMethod
 
-	case utils.Equal(text, DicStartCmd):
+	case isCommand(cmd, DicStartCmd):
 		message, err = p.gameDick(chat, user, messageID)
 		if err != nil {
 			return "", UnsupportedMethod, e.Wrap("can't get message from gameDick: ", err)
 		}
 		mthd = sendMessageMethod
 
-	case strings.HasPrefix(text, DickDuelCmd[0]):
+	case isCommand(strings.Split(cmd, " ")[0], DickDuelCmd) || isCommand(cmd, DickDuelCmd):
 		message, err = p.gameDuelDick(chat, messageID, user, user.Username)
 		if err != nil {
 			return "", UnsupportedMethod, e.Wrap("can't do gameDuelDick: ", err)
 		}
-		if utils.StringContains("@", text) {
-			textSplited := strings.Split(text, "@")
+		if utils.StringContains("@", cmd) {
+			textSplited := strings.Split(cmd, "@")
 			target := textSplited[len(textSplited)-1]
 			log.Printf("[INFO] @%s вызывает на дуель @%s", user.Username, target)
 			message, err = p.gameDuelDick(chat, messageID, user, target)
@@ -91,7 +94,7 @@ func (p *Processor) selectCommand(text string, chat *telegram.Chat, user *telegr
 		}
 		mthd = sendMessageMethod
 
-	case utils.Equal(text, XkcdCmd):
+	case isCommand(cmd, XkcdCmd):
 		var comics xkcd.Comics
 		comics, err = xkcd.RandomComics()
 		if err != nil {
@@ -100,16 +103,16 @@ func (p *Processor) selectCommand(text string, chat *telegram.Chat, user *telegr
 		message = comics.Img
 		mthd = sendPhotoMethod
 
-	case utils.Equal(text, AnecdotCmd):
+	case isCommand(cmd, AnecdotCmd):
 		message, err = jokesrv.Anecdot()
 		if err != nil {
 			return "", UnsupportedMethod, e.Wrap("can't get anecdot: ", err)
 		}
 		mthd = sendMessageMethod
-	case utils.Equal(text, FlipCmd):
+	case isCommand(cmd, FlipCmd):
 		message = RandomPhotoHinkOrRoom()
 		mthd = sendPhotoMethod
-	case utils.Equal(text, ScheduleCmd):
+	case isCommand(cmd, ScheduleCmd):
 		calendarID, err := p.storage.CalendarID(context.Background(), chat.ID)
 		if err != nil || calendarID == "" {
 			return "", UnsupportedMethod, e.Wrap("can't get calendarID: ", err)
@@ -121,11 +124,11 @@ func (p *Processor) selectCommand(text string, chat *telegram.Chat, user *telegr
 			message = fmt.Sprintf(msgErrorSendMessage, calendarID)
 		}
 		mthd = sendMessageMethod
-	case utils.Equal(strings.Split(text, " ")[0], AddCalendarIDCmd):
+	case isCommand(strings.Split(cmd, " ")[0], AddCalendarIDCmd):
 		if !p.isAdmin(user, chat.ID) {
 			return msgForbiddenCalendarUpdate, sendMessageMethod, nil
 		}
-		strs := strings.Split(text, " ")
+		strs := strings.Split(cmd, " ")
 		calendarID := ""
 		for _, str := range strs {
 			if len(str) > 0 {
@@ -165,6 +168,13 @@ func (p *Processor) isAdmin(user *telegram.User, chatID int) bool {
 		if user.ID == admin.ID {
 			return true
 		}
+	}
+	return false
+}
+
+func isCommand(cmd string, correctCmd string) bool {
+	if cmd == correctCmd || cmd == correctCmd+suffix {
+		return true
 	}
 	return false
 }
