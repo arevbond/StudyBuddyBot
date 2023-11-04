a6bff20647baa2ed7068cf7770209adcb41d6bfb
arevbond <bondarev.n17@yandex.ru>
2023-11-04 19:35:09 +0300

    Change Schedule func on 1 request to api

--- lib/schedule/schedule.go
+++ lib/schedule/schedule.go
@@ -6,24 +6,49 @@ import (
 	"time"
 )
 
-func Schedule(calendarID string) string {
-	result := "Расписание:\n\n"
-	for day := time.Monday; day <= time.Saturday; day++ {
-		cur := ScheduleByDay(day, calendarID)
-		if cur != "" {
-			result += cur + "\n"
+func Schedule(calendarID string) (string, error) {
+	result := "Расписание:\n"
+	dayToLessons, err := google_calendar.Lessons(calendarID)
+	if err != nil {
+		return "", err
+	}
+	for day := time.Sunday; day <= time.Saturday; day++ {
+		lessons := dayToLessons[day]
+		if len(lessons) > 0 {
+			result += "\n"
+			switch day {
+			case time.Monday:
+				result += "Понедельник"
+			case time.Tuesday:
+				result += "Вторник"
+			case time.Wednesday:
+				result += "Среда"
+			case time.Thursday:
+				result += "Четверг"
+			case time.Friday:
+				result += "Пятница"
+			case time.Saturday:
+				result += "Суббота"
+			}
+			result += "\n"
+			for _, l := range lessons {
+				result += fmt.Sprintf("%s - %s\n", l.Name, l.DateTime.Format("15:04"))
+			}
 		}
 	}
-	return result
+	return result, nil
 }
 
-func ScheduleByDay(day time.Weekday, calendarID string) string {
-	dayToLessons := google_calendar.Lessons(calendarID)
+func ScheduleByDay(day time.Weekday, calendarID string) (string, error) {
+	dayToLessons, err := google_calendar.Lessons(calendarID)
+	if err != nil {
+		return "", err
+	}
 	result := ""
 	lessons := dayToLessons[day]
 
 	if len(lessons) == 0 {
-		return ""
+		return "", nil
 	}
 
 	switch day {
@@ -45,5 +70,5 @@ func ScheduleByDay(day time.Weekday, calendarID string) string {
 	for _, l := range lessons {
 		result += fmt.Sprintf("%s - %s\n", l.Name, l.DateTime.Format("15:04"))
 	}
-	return result
+	return result, nil
 }
