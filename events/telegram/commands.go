aed620688c24f26251e482d26a4581a67291a9a0
arevbond <bondarev.n17@yandex.ru>
2024-01-25 13:12:01 +0300

    Refactor commands.go

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -4,54 +4,71 @@ const (
 	suffix = "@ics_useful_bot"
 )
 
-const ()
-
+// dick
 const (
-	HelpCmd = "/help"
-
 	DicStartCmd = "/dick"
 	DickTopCmd  = "/top_dick"
 	DickDuelCmd = "/duel"
+	GetHPCmd    = "/hp"
+)
 
+// gay
+const (
 	GayStartCmd = "/gay"
 	GayTopCmd   = "/top_gay"
+)
 
+// calendar
+const (
 	AddCalendarIDCmd = "/add_calendar"
+	ScheduleCmd      = "/schedule"
+)
 
-	ScheduleCmd = "/schedule"
-	AnecdotCmd  = "/joke"
-	XkcdCmd     = "/xkcd"
-	FlipCmd     = "/flip"
-
-	AllCmd = "/all"
-
+// homework
+const (
 	AddHomeworkCmd    = "/add"
 	GetHomeworkCmd    = "/get"
 	DeleteHomeworkCmd = "/delete"
 	CancelHomeworkCmd = "/cancel"
+)
 
-	GetMyStatsCmd   = "/my_stats"
-	GetChatStatsCmd = "/chat_stats"
+// quiz
+const (
+	StartQuizCmd = "/start_quiz"
+)
 
-	GetChatIDCmd = "/chat_id"
-
-	GetHPCmd = "/hp"
-
-	// admins commands
-	ChangeDickCmd         = "/change_dick"
-	SendMessageByAdminCmd = "/send_message"
-
+// auction
+const (
 	StartAuctionCmd  = "/start_auction"
 	FinishAuctionCmd = "/finish_auction"
 	AddDepositCmd    = "/deposit"
 	AuctionCmd       = "/auction"
+)
 
+// stats
+const (
+	GetMyStatsCmd   = "/my_stats"
+	GetChatStatsCmd = "/chat_stats"
+)
+
+// admins
+const (
+	ChangeDickCmd         = "/change_dick"
+	SendMessageByAdminCmd = "/send_message"
+)
+
+// utils
+const (
+	AllCmd       = "/all"
+	HelpCmd      = "/help"
+	AnecdotCmd   = "/joke"
+	XkcdCmd      = "/xkcd"
+	FlipCmd      = "/flip"
+	GetChatIDCmd = "/chat_id"
+)
+
+// other
+const (
 	// HOLIDAY
 	HolidayCmd = "/holiday"
-
-	// QUIZ
-	StartQuizCmd = "/start_quiz"
-
-	// SHOP
-	ShopCmd = "/shop"
 )
