b5dcb57c79ca63c4c8c4697bb14a424df3a3e2b0
arevbond <bondarev.n17@yandex.ru>
2024-03-12 17:59:05 +0300

    Change architecture in cmd_gay

--- events/telegram/cmd_gay.go
+++ events/telegram/cmd_gay.go
@@ -11,28 +11,13 @@ import (
 	"time"
 )
 
-// gayExec предоставляет метод Exec для выполнения /gay.
-type gayExec string
-
-// Exec: /gay - определяет случайного пидора в чате среди админов чата.
-func (a gayExec) Exec(p *Processor, inMessage string, user *telegram.User, chat *telegram.Chat,
-	userStats *storage.DBUserStat, messageID int) (*Response, error) {
-
-	message, err := p.gameGay(chat.ID)
-	if err != nil {
-		return nil, e.Wrap("can't get message from gameGay: ", err)
-	}
-	mthd := sendMessageMethod
-	return &Response{message: message, method: mthd, replyMessageId: -1}, nil
-}
-
 // topGaysExec предоставляет метод Exec для вывода топа пидоров.
 type topGaysExec string
 
 // Exec: /top_gay - выводит список участников чата и их кол-во становления пидором дня.
 func (a topGaysExec) Exec(p *Processor, inMessage string, user *telegram.User, chat *telegram.Chat,
 	userStats *storage.DBUserStat, messageID int) (*Response, error) {
-	message, err := p.topGays(chat.ID)
+	message, err := a.topGays(chat.ID, p)
 	if err != nil {
 		return nil, e.Wrap("can't do GayTop: ", err)
 	}
@@ -40,25 +25,83 @@ func (a topGaysExec) Exec(p *Processor, inMessage string, user *telegram.User, c
 	return &Response{message: message, method: mthd, replyMessageId: -1}, nil
 }
 
+// topGaysExec возвращает список всех админов и сколько раз они были пидорами.
+func (a topGaysExec) topGays(chatID int, p *Processor) (message string, err error) {
+	admins, err := p.tg.ChatAdministrators(chatID)
+
+	if err != nil {
+		return "", e.Wrap("[ERROR] can't get chat administrators: ", err)
+	}
+
+	type userWithCount struct {
+		name  string
+		count int
+	}
+	users := []userWithCount{}
+
+	for _, u := range admins {
+		dbUser, err := p.storage.GetUser(context.Background(), u.ID, chatID)
+		if err == storage.ErrUserNotExist {
+			dbUser, err = p.createNewUserInDB(chatID, &u)
+			if err != nil {
+				return "", e.Wrap("can't create new user in db; in 'createNewGayOfDay'", err)
+			}
+		}
+		dbUserStat, err := p.storage.GetUserStats(context.Background(), dbUser)
+		if err != nil {
+			return "", err
+		}
+		users = append(users, userWithCount{
+			name:  dbUser.FirstName + " " + dbUser.LastName,
+			count: dbUserStat.GayCount,
+		})
+	}
+
+	sort.Slice(users, func(i, j int) bool {
+		return users[i].count > users[j].count
+	})
+
+	result := "Рейтинг пидоров: \n\n"
+	for i, user := range users {
+		result += fmt.Sprintf("%d. %s %d раз(а)\n", i+1, user.name, user.count)
+	}
+	return result, nil
+}
+
+// gayExec предоставляет метод Exec для выполнения /gay.
+type gayExec string
+
+// Exec: /gay - определяет случайного пидора в чате среди админов чата.
+func (g gayExec) Exec(p *Processor, inMessage string, user *telegram.User, chat *telegram.Chat,
+	userStats *storage.DBUserStat, messageID int) (*Response, error) {
+
+	message, err := g.gameGay(chat.ID, p)
+	if err != nil {
+		return nil, e.Wrap("can't get message from gameGay: ", err)
+	}
+	mthd := sendMessageMethod
+	return &Response{message: message, method: mthd, replyMessageId: -1}, nil
+}
+
 // gameGay определяет пидора дня среди администратора и возвращает сообщение для чата.
-func (p *Processor) gameGay(chatID int) (string, error) {
+func (g gayExec) gameGay(chatID int, p *Processor) (string, error) {
 	admins, err := p.tg.ChatAdministrators(chatID)
 	if err != nil {
 		return "", e.Wrap("can't get chat administrators: ", err)
 	}
 
-	gay, isCreated, err := p.gayOfDay(chatID, admins)
+	gay, isCreated, err := g.gayOfDay(chatID, admins, p)
 	if err != nil {
 		return "", e.Wrap("can't initial gameGay", err)
 	}
-	name, hasUsername, err := p.getGayName(gay)
+	name, hasUsername, err := g.getGayName(gay, p.storage)
 	if err != nil {
 		return "", e.Wrap("can't get gay name", err)
 	}
-	return formatOutputGay(hasUsername, isCreated, name), nil
+	return g.formatOutputGay(hasUsername, isCreated, name), nil
 }
 
-func formatOutputGay(hasUsername bool, isCreated bool, name string) string {
+func (g gayExec) formatOutputGay(hasUsername bool, isCreated bool, name string) string {
 	if isCreated {
 		if hasUsername {
 			return fmt.Sprintf(msgNewGayOfDayUsername, name)
@@ -72,31 +115,31 @@ func formatOutputGay(hasUsername bool, isCreated bool, name string) string {
 	return fmt.Sprintf(msgCurrentGayOfDayFullName, name)
 }
 
-func (p *Processor) gayOfDay(chatID int, admins []telegram.User) (*storage.DBGay, bool, error) {
+func (g gayExec) gayOfDay(chatID int, admins []telegram.User, p *Processor) (*storage.DBGay, bool, error) {
 	gay, err := p.storage.GetGayOfDay(context.Background(), chatID)
 	if err == storage.ErrUserNotExist {
-		gay, err = p.createNewGayOfDay(chatID, admins)
+		gay, err = g.createNewGayOfDay(chatID, admins, p)
 		return gay, true, nil
 	} else if err != nil {
 		return nil, false, e.Wrap("can't get gay of day: ", err)
 	}
-	if gayIsOld(gay) {
+	if g.gayIsOld(gay) {
 		err = p.storage.RemoveGayOfDay(context.Background(), chatID)
 		if err != nil {
 			return nil, false, err
 		}
-		gay, err = p.createNewGayOfDay(chatID, admins)
+		gay, err = g.createNewGayOfDay(chatID, admins, p)
 		return gay, true, nil
 	}
 	return gay, false, nil
 }
 
-func gayIsOld(gay *storage.DBGay) bool {
+func (g gayExec) gayIsOld(gay *storage.DBGay) bool {
 	return (gay.CreatedAt.Month() == time.Now().Month() && gay.CreatedAt.Day() < time.Now().Day()) || gay.CreatedAt.Month() < time.Now().Month() || gay.CreatedAt.Year() < time.Now().Year()
 }
 
-func (p *Processor) getGayName(gay *storage.DBGay) (string, bool, error) {
-	dbUser, err := p.storage.GetUser(context.Background(), gay.TgID, gay.ChatID)
+func (g gayExec) getGayName(gay *storage.DBGay, db storage.Storage) (string, bool, error) {
+	dbUser, err := db.GetUser(context.Background(), gay.TgID, gay.ChatID)
 	if dbUser.Username != "" {
 		return dbUser.Username, true, nil
 	}
@@ -107,7 +150,7 @@ func (p *Processor) getGayName(gay *storage.DBGay) (string, bool, error) {
 }
 
 // createNewGayOfDay создаёт пидора дня.
-func (p *Processor) createNewGayOfDay(chatID int, admins []telegram.User) (*storage.DBGay, error) {
+func (g gayExec) createNewGayOfDay(chatID int, admins []telegram.User, p *Processor) (*storage.DBGay, error) {
 	var user *telegram.User
 	for {
 		n := rand.Intn(len(admins))
@@ -148,47 +191,4 @@ func (p *Processor) createNewGayOfDay(chatID int, admins []telegram.User) (*stor
 	}
 
 	return gay, nil
-}
-
-// topGaysExec возвращает список всех админов и сколько раз они были пидорами.
-func (p *Processor) topGays(chatID int) (message string, err error) {
-	admins, err := p.tg.ChatAdministrators(chatID)
-
-	if err != nil {
-		return "", e.Wrap("[ERROR] can't get chat administrators: ", err)
-	}
-
-	type userWithCount struct {
-		name  string
-		count int
-	}
-	users := []userWithCount{}
-
-	for _, u := range admins {
-		dbUser, err := p.storage.GetUser(context.Background(), u.ID, chatID)
-		if err == storage.ErrUserNotExist {
-			dbUser, err = p.createNewUserInDB(chatID, &u)
-			if err != nil {
-				return "", e.Wrap("can't create new user in db; in 'createNewGayOfDay'", err)
-			}
-		}
-		dbUserStat, err := p.storage.GetUserStats(context.Background(), dbUser)
-		if err != nil {
-			return "", err
-		}
-		users = append(users, userWithCount{
-			name:  dbUser.FirstName + " " + dbUser.LastName,
-			count: dbUserStat.GayCount,
-		})
-	}
-
-	sort.Slice(users, func(i, j int) bool {
-		return users[i].count > users[j].count
-	})
-	
-	result := "Рейтинг пидоров: \n\n"
-	for i, user := range users {
-		result += fmt.Sprintf("%d. %s %d раз(а)\n", i+1, user.name, user.count)
-	}
-	return result, nil
 }
